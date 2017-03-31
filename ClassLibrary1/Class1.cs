7bc516849f17327295a661ca60c3aebbd10b2a21
Richard Lander <rlander@microsoft.com>
2017-03-31 10:07:18 -0700

    Add project that use PackageFallBack

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using GalaSoft.MvvmLight.Messaging;
+using Cimbalino.Toolkit.Foundation;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static (Messenger m, Point p) Test1()
+        {
+            var m = new Messenger();
+            var p = new Point(1, 2);
+            return (m, p);
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var val = ClassLibrary1.Class1.Test1();
+            Console.WriteLine($"Messenger: {val.m.ToString()}; Cimbalino: {val.p.X.ToString()}");
+            
+        }
+    }
+}
